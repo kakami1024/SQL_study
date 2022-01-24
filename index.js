5f6e92ce325eeaec369bab76df990542543dbed1
kakami1024 <kakami1024@gmail.com>
2022-01-24 10:34:05 +0900

    0124'index.js 파일 삭제'

--- index.js
+++
@@ -1,5 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-console.log(process.env.DATABASE_USERNAME);
-console.log(process.env.DATABASE_PASSWORD);
-console.log(process.env.DATABASE_NAME);
