dd498fd0aea8da743c35ffe10e19a6224e64330c
kakami1024 <kakami1024@gmail.com>
2022-01-25 08:25:51 +0900

    0125'SQL 완료'

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -3,9 +3,6 @@ USE learnmysql;
 
 /* DESIGN SCHEMA FOR REQUIREMENT */
 
--- PART 3:
--- 여기 user 테이블 생성 SQL 구문이 있습니다. user 테이블을 참고해서, content 테이블 생성 SQL 구문을 완성하세요.
-
 CREATE TABLE `user` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) not NULL,
@@ -13,36 +10,36 @@ CREATE TABLE `user` (
 );
 
 CREATE TABLE `content` (
-  -- TODO: `id`
-  `id`int not NULL PRIMARY KEY AUTO_INCREMENT,
-  -- TODO: `title`
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `title` varchar(255) not NULL,
-  -- TODO: `body`
   `body` varchar(255) not NULL,
-  `created_at` timestamp not NULL DEFAULT CURRENT_TIMESTAMP,
-  `userId` int,
-  FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+  `created_at` timestamp not NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- PART 5:
--- 아래 주석을 제거하고 category, content_category, role 테이블을 만드세요.
+ALTER TABLE `content` ADD userId int;
+ALTER TABLE `content` ADD FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
 
-/*
+
 CREATE TABLE `category` (
-  -- TODO:
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) not NULL
 );
 
 
 CREATE TABLE `content_category` (
-  -- TODO:
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `contentId` int NOT NULL, 
+  `categoryId` int NOT NULL
 );
 
+
+ALTER TABLE `content_category` ADD FOREIGN KEY (`contentId`) REFERENCES `content` (`id`);
+ALTER TABLE `content_category` ADD FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`);
 
 CREATE TABLE `role` (
-  -- TODO:
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) not NULL
 );
-
 
 ALTER TABLE `user` ADD roleId int;
 ALTER TABLE `user` ADD FOREIGN KEY (`roleId`) REFERENCES `role` (`id`);
-*/
